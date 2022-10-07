5865ba42c4955384ce5f1d09c4bfedb2cab33f39
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-07 10:28:06 +0100

    commmit

--- src/App.js
+++ src/App.js
@@ -2,44 +2,31 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Card from './components/Card';
+import Data from './components/Data';
 import './App.css';
 
 
 function App() {
+  const cards = Data.map(card =>{
+
+    return <Card 
+    img= {card.Img}
+    status={card.status}
+    rating={card.stats.rating}
+    reviewCount={card.stats.reviewCount}
+    country={card.location}
+    title={card.title}
+    price={card.price}
+    />
+  })
+
   return (
     <div className="App">
-
       <Header />
       <div className="space">
           <Main />
           <div className="card-container">  
-            <Card 
-                img= "pic-1.png"
-                status="SOlD OUT"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />    
-            <Card 
-                img= "pic-2.png"
-                status="ON SALE"
-                rating="5.0"
-                reviewCount={30}
-                country="USA"
-                title="Learn wedding photography"
-                price={125}
-            />    
-            <Card 
-                img= "pic-3.png"
-                status="SOlD OUT"
-                rating="4.8"
-                reviewCount={2}
-                country="USA"
-                title="Group Mountain Biking"
-                price={50}
-            />    
+            {cards}
           </div>
       </div>     
     </div>
@@ -47,3 +34,31 @@ function App() {
 }
 
 export default App;
+
+// {/* <Card 
+//                 img= "pic-1.png"
+//                 status="SOlD OUT"
+//                 rating="5.0"
+//                 reviewCount={6}
+//                 country="USA"
+//                 title="Life Lessons with Katie Zaferes"
+//                 price={136}
+//             />    
+//             <Card 
+//                 img= "pic-2.png"
+//                 status="ON SALE"
+//                 rating="5.0"
+//                 reviewCount={30}
+//                 country="USA"
+//                 title="Learn wedding photography"
+//                 price={125}
+//             />    
+//             <Card 
+//                 img= "pic-3.png"
+//                 status="SOlD OUT"
+//                 rating="4.8"
+//                 reviewCount={2}
+//                 country="USA"
+//                 title="Group Mountain Biking"
+//                 price={50}
+//             />     */}
