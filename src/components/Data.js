daa957f490a0673945730b239ee5094f403b252e
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-08 00:52:10 +0100

    more comnit

--- src/components/Data.js
+++ src/components/Data.js
@@ -29,20 +29,7 @@ const Data = [
         status: "ON SALE",
         openSpots: 27,
     },
-    {
-        id: 4,
-        title: "Learn Wedding Photography",
-        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-        price: 125,
-        Img: "pic-2.png",
-        stats: {
-            rating: 5.0,
-            reviewCount: 30
-        },
-        location: "Online",
-        status: "ON SALE",
-        openSpots: 27,
-    },
+
     {
         id: 3,
         title: "Group Mountain Biking",
